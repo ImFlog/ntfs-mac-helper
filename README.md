472ed05eeae339353c636f07a6c4bcf5f0eb6385
Garcia Florian <garcia.floriian@gmail.com>
2017-06-28 23:37:13 +0200

    Working version

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # ntfs-mac-helper
-Install all the requirements to enable read / write to ntfs drives.
+Install all the requirements to enable read / write to ntfs drives on macos, for free.
 
 ## How-to
 1. Download the shell script
 2. chmod +x ntfs.sh
 3. ./ntfs.sh
-4. Enjoy !
+4. Follow the instruction (only a few)
+
+The process can take a few minutes, don't panic.
+You may be asked for your password, some actions requires administrator privileges.
 
 ## Troubleshooting
 Do not hesitates to create an issue if the script doesn't work for you, I cannot guarantee to answer fast though.
 
 ## Disclaimers
 I am not responsible for any damage that could happen while using this script.
+Use It at your own risk !
--- ntfs.sh
+++ ntfs.sh
@@ -2,24 +2,33 @@
 
 # Test if SIP is disabled
 sip_status="$(csrutil status)"
-if [[ $string == *"disabled"* ]]; then
+if [[ $sip_status != *"disabled"* ]]; then
     echo "Please disable SIP before running this script"
     exit 1
 else
+    # Install developer tools for xcode
+    xcode-select --install
+    read -p "Press any key when xcode is installed"
+
     # OSX fuse part
     curl -sSL https://github.com/osxfuse/osxfuse/releases/download/osxfuse-3.6.0/osxfuse-3.6.0.dmg -o osxfuse.dmg
-    VOLUME=$(hdiutil attach osxfuse.dmg | tail -1 | awk '{print $1}')
-    cp -r "$VOLUME/"*.app /Applications/
-    diskutil unmount "$VOLUME"
+    hdiutil attach osxfuse.dmg
+    sudo installer -pkg /Volumes/FUSE*/*.pkg -target /
+    diskutil unmount /Volumes/FUSE*
+    rm osxfuse.dmg
 
     # Homebrew part
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew cask install osxfuse
-    brew install homebrew/fuse/ntfs-3g
+    brew install ntfs-3g
 
     # Effective volume mount
-    sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.original
-    sudo ln -s /usr/local/sbin/mount_ntfs /sbin/mount_ntfs
+    read -p "If you already run this script after this point, you shouldn't backup"
+    read -e -p "Do you need to backup ? (yes/no)" "no" choice
 
-    # TODO : cleanup
+    if [[ $choice == "yes" ]]; then
+        sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.original
+    fi
+    # No harm here
+    sudo ln -s /usr/local/sbin/mount_ntfs /sbin/mount_ntfs
 fi
